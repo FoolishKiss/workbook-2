9f2ecf053016121dbc46c44adb12088e8718e7e0
Brian Miller <Bmiller995@gmail.com>
2025-04-17 09:13:43 -0500

    added system out print in if statement

--- FullNameParser/src/main/java/com/pluralsight/NameParserApplication.java
+++ FullNameParser/src/main/java/com/pluralsight/NameParserApplication.java
@@ -15,28 +15,31 @@ public class NameParserApplication {
         // takes user input and puts it in variable name
         String name = userName.nextLine().trim();
 
-        // uses split to split name
+        // uses split to split name by "spaces"
         String[] nameParts = name.split(" ");
 
         // variables
         String firstName = "";
-        String middleName = "";
+        String middleName = "(none)";
         String lastName = "";
 
         // checks to see if user has 2 or 3 parts
         if (nameParts.length == 2) {
             firstName = nameParts[0];
             lastName = nameParts[1];
+            System.out.println("\nFirst Name: " + firstName);
+            System.out.println("Middle Name: (none)");
+            System.out.println("Last Name: " + lastName);
         } else if (nameParts.length == 3) {
             firstName = nameParts[0];
             middleName = nameParts[1];
             lastName = nameParts[2];
+            System.out.println("\nFirst Name: " + firstName);
+            System.out.println("Middle Name: " + middleName);
+            System.out.println("Last Name: " + lastName);
+        }else {
+            System.out.println("that's not right bro");
         }
-
-        // displays users name parsed out
-        System.out.println("\nFirst Name: " + firstName);
-        System.out.println("Middle Name: " + middleName);
-        System.out.println("Last Name: " + lastName);
 
 
     }
